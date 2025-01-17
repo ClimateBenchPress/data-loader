72b91cbd3d9ef80f16fb89ff9bebaae6a9c1eaea
Tim Reichelt <treichelt6@gmail.com>
2025-01-17 16:02:18 +0000

    Add tiny datasets for debugging

---
+++ scripts/cmip6_tiny.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+import fsspec
+import pandas as pd
+import xarray as xr
+
+import climatebenchpress.data_loader
+
+name = Path(__file__).stem
+path = climatebenchpress.data_loader.get_dataset_path(name)
+
+if not path.exists():
+    df = pd.read_csv(
+        "https://storage.googleapis.com/cmip6/cmip6-zarr-consolidated-stores.csv"
+    )
+
+    ssp_test = "ssp585"
+    model = "ACCESS-ESM1-5"
+
+    df_ta = df.query(
+        f"variable_id == 'ta' & experiment_id == '{ssp_test}' & "
+        f"source_id == '{model}' & table_id == 'Amon'"
+    )
+
+    zstore = df_ta.zstore.values[-1]
+    mapper = fsspec.get_mapper(zstore)
+
+    ds = xr.open_zarr(mapper, consolidated=True)
+    # Create a tiny dataset for testing.
+    ds = ds.isel(time=slice(0, 3))
+
+    climatebenchpress.data_loader.download_canonicalized_dataset(ds, name)
+
+ds = xr.open_dataset(path, chunks=dict())
+
+for v, da in ds.items():
+    print(f"- {v}: {da.dims}")
---
+++ scripts/era5_tiny.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+import xarray as xr
+
+import climatebenchpress.data_loader
+
+ERA5_GCP_PATH = (
+    "gs://gcp-public-data-arco-era5/ar/1959-2022-full_37-1h-0p25deg-chunk-1.zarr-v2"
+)
+
+name = Path(__file__).stem
+path = climatebenchpress.data_loader.get_dataset_path(name)
+
+if not path.exists():
+    era5 = xr.open_zarr(ERA5_GCP_PATH, chunks={"time": 48}, consolidated=True)
+    ds = era5.sel(time=slice("2020-03-01", "2020-03-07"))[
+        [
+            "mean_sea_level_pressure",
+            "10m_u_component_of_wind",
+            "10m_v_component_of_wind",
+        ]
+    ]
+    # Create a tiny dataset for testing.
+    ds = ds.isel(time=slice(0, 3))
+
+    # Needed to make the dataset CF-compliant.
+    ds.time.attrs["standard_name"] = "time"
+    climatebenchpress.data_loader.download_canonicalized_dataset(ds, name)
+
+ds = xr.open_dataset(path, chunks=dict())
+
+for v, da in ds.items():
+    print(f"- {v}: {da.dims}")
