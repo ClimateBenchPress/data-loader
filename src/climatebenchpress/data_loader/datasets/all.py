48db7e5bbc83a429ca3fdbdb4d0a68f24fe94131
Tim Reichelt <treichelt6@gmail.com>
2025-03-05 15:56:22 +0000

    Add NextGEMS ICON model data source

--- src/climatebenchpress/data_loader/datasets/all.py
+++ src/climatebenchpress/data_loader/datasets/all.py
@@ -3,3 +3,4 @@
 from .cmip6.all import *
 from .era5 import *
 from .esa_biomass_cci import *
+from .nextgems import *
---
+++ src/climatebenchpress/data_loader/datasets/nextgems.py
@@ -0,0 +1,79 @@
+__all__ = ["NextGemsDataset"]
+
+from pathlib import Path
+
+import healpy
+import intake
+import numpy as np
+import xarray as xr
+
+from .. import (
+    monitor,
+    open_downloaded_canonicalized_dataset,
+    open_downloaded_tiny_canonicalized_dataset,
+)
+from .abc import Dataset
+
+NEXTGEMS_CATALOG = "https://data.nextgems-h2020.eu/online.yaml"
+# Zoom level for the HealPIX grid (https://easy.gems.dkrz.de/Processing/healpix/index.html#healpix-spatial-resolution).
+# See https://swift.dkrz.de/v1/dkrz_b381d76e-63d7-4aeb-96f0-dfd91e102d40/nextgems_prefinal/nextgems_prefinal_output.html
+# for which variables are available at a given zoom level and time resolution.
+ZOOM = 9
+# Allowed values: "P1D" (daily), "PT3H" (3-hourly), "PT15M" (15-minute).
+TIME_RESOLUTION = "PT3H"
+
+NUM_LON = 2880
+NUM_LAT = 1440
+
+
+class NextGemsDataset(Dataset):
+    name = "nextgems-icon"
+
+    @staticmethod
+    def download(download_path: Path, progress: bool = True):
+        downloadfile = download_path / "download.zarr"
+        donefile = downloadfile.parent / (downloadfile.name + ".done")
+        if donefile.exists():
+            return
+
+        cat = intake.open_catalog(NEXTGEMS_CATALOG)
+        icon = cat["ICON.ngc4008"](
+            zoom=ZOOM, time=TIME_RESOLUTION, chunks=dict()
+        ).to_dask()
+
+        ds = icon[["rlut"]].sel(time=slice("2020-03-01", "2020-03-07"))
+        # Regrid the data to 0.125 degree resolution.
+        # NOTE: This is using nearest neighbour interpolation. We need to do some
+        #       quality checks to ensure we don't get any significant aliasing
+        #       artifacts as the result of interpolation. For more details:
+        #       https://easy.gems.dkrz.de/Processing/healpix/lonlat_remap.html.
+        idx = _get_nn_lon_lat_index(
+            2**ZOOM, np.linspace(-180, 180, 2880), np.linspace(-90, 90, 1440)
+        )
+        ds = ds.isel(cell=idx).chunk({"time": 1, "lat": NUM_LAT, "lon": NUM_LON})
+        ds.lon.attrs["axis"] = "X"
+        ds.lat.attrs["axis"] = "Y"
+
+        with monitor.progress_bar(progress):
+            ds.to_zarr(downloadfile, mode="w", encoding=dict(), compute=False).compute()
+        donefile.touch()
+
+    @staticmethod
+    def open(download_path: Path) -> xr.Dataset:
+        return xr.open_zarr(download_path / "download.zarr")
+
+
+def _get_nn_lon_lat_index(nside, lons, lats):
+    lons2, lats2 = np.meshgrid(lons, lats)
+    return xr.DataArray(
+        healpy.ang2pix(nside, lons2, lats2, nest=True, lonlat=True),
+        coords=[("lat", lats), ("lon", lons)],
+    )
+
+
+if __name__ == "__main__":
+    ds = open_downloaded_canonicalized_dataset(NextGemsDataset)
+    open_downloaded_tiny_canonicalized_dataset(NextGemsDataset)
+
+    for v, da in ds.items():
+        print(f"- {v}: {da.dims}")
